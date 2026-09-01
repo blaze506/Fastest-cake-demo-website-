00a9e051f5a00983291001ff4bbed1657403eb94
blaze506 <destinyochoche1@gmail.com>
2026-09-01 02:00:33 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = "fastest-cakes-v1";
+const CACHE_NAME = "fastest-cakes-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
